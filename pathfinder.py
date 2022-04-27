4b6f8446fd43adb809048f7b5840b0a835830989
Gerardo Ayes <gerardoayes92@gmail.com>
2022-04-27 15:52:25 -0400

    almost done

--- pathfinder.py
+++ pathfinder.py
@@ -1,9 +1,10 @@
 from PIL import Image
 from PIL import ImageColor
 import numpy as np
+import random
 
 
-class TextFile:
+class TopoMap:
     # file_name is name of text file with elevation data
     def __init__(self, name_txt):
         self.name_txt = name_txt
@@ -31,28 +32,58 @@ class TextFile:
         self.image = Image.new('RGBA', self.size_data, 'white')
         self.image.save(self.name_png)
 
-        for x in range(len(self.data)):
-            for y in range(len(self.data[x])):
-                current_data_element = self.data[x][y]
+        for row in range(len(self.data)):
+            for col in range(len(self.data[row])):
+                current_data_element = self.data[row][col]
                 grayscale = (current_data_element - self.min_data) // ((self.max_data - self.min_data)//256 + 1)
-                self.image.putpixel((y, x), (grayscale, grayscale, grayscale, 255))
+                self.image.putpixel((col, row), (grayscale, grayscale, grayscale, 255))
         self.image.save(self.name_png)
 
-    def greedy_path(self):
-        right = 
+    def find_greedy_path(self, starting_row):
+        row = starting_row
+        col = 0
+        self.path = []
+        current = self.data[row][col]
+
+        while col < len(self.data[row])-1:
+            right_up = self.data[row-1][col+1]
+            right_down = self.data[row+1][col+1]
+            right = self.data[row][col+1]
+
+            diffs = [abs(current-right), abs(current-right_down), abs(current-right_up)]
+            min_diff = min(diffs)
+
+            if min_diff == diffs[0]:
+                current = right
+            elif diffs[1] == diffs[2] == min_diff:
+                current = diffs[random.randint(1,2)]
+                if current == right_down:
+                    row += 1
+                else:
+                    row -= 1
+            elif min_diff == diffs[1]:
+                current = right_down
+                row += 1
+            elif min_diff == diffs[2]:
+                current = right_up
+                row -= 1
+
+            self.path.append((col, row))
+            col += 1
 
     def chart_greedy_path(self):
-        self.test_answer = [(0,6), (1,5), (2,4), (3,3), (4,2), (5,3), (6,2), (7,2), (8,3), (9,4), (10,5), (11,5), (12,5), (13,6)]
-        for i in range(len(self.test_answer)):
-            self.image.putpixel(self.test_answer[i], ImageColor.getcolor('red', 'RGBA'))
+        # self.test_answer = [(0,6), (1,5), (2,4), (3,3), (4,2), (5,3), (6,2), (7,2), (8,3), (9,4), (10,5), (11,5), (12,5), (13,6)]
+        for i in range(len(self.path)):
+            self.image.putpixel(self.path[i], ImageColor.getcolor('red', 'RGBA'))
         self.image.save(self.name_png)
 
-# elevation_small = TextFile('elevation_small.txt')
+# elevation_small = TopoMap('elevation_small.txt')
 # elevation_small.txt_to_png()
 
 
-elevation_test = TextFile('elevation_test.txt')
+elevation_test = TopoMap('elevation_test.txt')
 elevation_test.txt_to_png()
+elevation_test.find_greedy_path(6)
 elevation_test.chart_greedy_path()
 
 
